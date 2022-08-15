ad784e856e9ffed258d279c536d5c7ecf7bca5f5
mashroorzisan <mashroorzisan@gmail.com>
2022-08-15 12:30:28 +0600

    third practice on creating commenting

---
+++ js/twitter-comment-function.js
@@ -0,0 +1,6 @@
+// 0.styling with js
+document.getElementById('h1').style.textAlign = 'center';
+document.getElementById('h1').style.color = '#B9345A';
+document.getElementById('span1').style.marginRight = '30px';
+document.getElementById('span1').style.color = 'gray';
+document.getElementById('span2').style.color = 'gray';
